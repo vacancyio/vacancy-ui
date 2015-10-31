321e391593033356385ebaab9aa2968b17b14567
Owain Lewis <owain@owainlewis.com>
2015-10-31 17:08:42 +0000

    Update ControllerTestHelpers.scala

--- test/app/controllers/ControllerTestHelpers.scala
+++ test/app/controllers/ControllerTestHelpers.scala
@@ -1,10 +1,10 @@
 package app.controllers
 
+import play.api.test._
+import play.api.test.Helpers._
+  
 trait ControllerTestHelpers {
 
-  import play.api.test._
-  import play.api.test.Helpers._
-
-  def getStatus(url: String): Option[Int] = route(FakeRequest(GET, url)) map status
-
+  def getStatus(url: String): Option[Int] = 
+    route(FakeRequest(GET, url)) map status
 }
