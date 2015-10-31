c60f662c280bc65e6e573c787973688da4bdd302
Owain Lewis <owain@owainlewis.com>
2015-10-31 17:09:08 +0000

    Update HelpersSpec.scala

--- test/app/helpers/HelpersSpec.scala
+++ test/app/helpers/HelpersSpec.scala
@@ -7,12 +7,11 @@ import org.specs2.mutable.Specification
 class HelpersSpec extends Specification {
 
   "The pluralize method" should {
+    
     "return zero length unchanged" in {
+      
       Format.pluralize(1, "foo") mustEqual "foo"
       Format.pluralize(2, "bar") mustEqual "bars"
-
     }
-
   }
-
 }
