f280afa4a8708a48e6ebc9d1415924e164b478dd
Owain Lewis <owain@owainlewis.com>
2015-11-02 20:53:33 +0000

    Update Location.scala

--- app/helpers/Location.scala
+++ app/helpers/Location.scala
@@ -2,7 +2,7 @@ package helpers
 
 object Location {
 
-  def nameFromISO2(iso2: String) = iso2 match {
+  def nameFromISO2(iso2: String): String = iso2 match {
     case "GB" => "United Kingdom"
     case "US" => "United States"
     case x    => x
