bc37b6231ffb1ea9b2f2aa4673a22d67a6f9c5ec
Owain Lewis <owain@owainlewis.com>
2015-11-03 11:17:56 +0000

    Update BasicAuthAction.scala

--- app/controllers/api/BasicAuthAction.scala
+++ app/controllers/api/BasicAuthAction.scala
@@ -10,7 +10,6 @@ object BasicAuthAction extends ActionBuilder[Request] with ActionFilter[Request]
     Results.Unauthorized.withHeaders("WWW-Authenticate" -> "Basic realm=Unauthorized")
 
   def filter[A](request: Request[A]): Future[Option[Result]] = {
-
     val result = request.headers.get("Authorization") map { authHeader =>
       val (email, pass) = decodeBasicAuth(authHeader)
       if (Employer.authenticate(email, pass)) None else Some(unauthorized)
