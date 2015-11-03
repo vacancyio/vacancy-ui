373819f21ffa025b87e9b5c917c3b363eae986d8
Owain Lewis <owain@owainlewis.com>
2015-11-03 11:19:26 +0000

    Update EmployerSecuredAction.scala

--- app/security/EmployerSecuredAction.scala
+++ app/security/EmployerSecuredAction.scala
@@ -6,19 +6,17 @@ import model._
 import play.api.mvc._
 
 trait EmployerSecuredAction {
-
   private def email(request: RequestHeader): Option[String] =
     request.session.get("email")
 
   def onUnauthorized(request: RequestHeader): Result =
     Results.Redirect(routes.EmployerAuthentication.login())
 
-  def withAuth(f: => String => Request[AnyContent] => Result): EssentialAction = {
+  def withAuth(f: => String => Request[AnyContent] => Result): EssentialAction =
     Security.Authenticated(email, onUnauthorized) { user =>
       Action(request => f(user)(request))
     }
-  }
-
+  
   def withEmployer(f: Employer => Request[AnyContent] => Result): EssentialAction =
     withAuth { email => implicit request =>
       EmployerRepository.findOneByEmail(email).fold(onUnauthorized(request))(f(_)(request))
