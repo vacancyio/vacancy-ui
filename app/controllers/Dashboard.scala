f8b30fd26fa12f669d8a0ad2c17e3c79dd421118
Owain Lewis <owain@owainlewis.com>
2015-11-03 11:24:46 +0000

    Update Dashboard.scala

--- app/controllers/Dashboard.scala
+++ app/controllers/Dashboard.scala
@@ -5,6 +5,7 @@ import repository.JobRepository
 import security.EmployerSecuredAction
 
 class Dashboard extends Controller with EmployerSecuredAction {
+  
   def employers = withEmployer { employer => { implicit request =>
     val jobs = employer.id map { id => JobRepository.forEmployer(id) } getOrElse Nil
     Ok(views.html.employers.dashboards.index(employer, jobs))
