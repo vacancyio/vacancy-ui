8548a5b09c862563f7f6e7651f7b2254a13a33c3
Owain Lewis <owain@owainlewis.com>
2015-11-08 15:08:28 +0000

    adds static caching

--- app/controllers/Static.scala
+++ app/controllers/Static.scala
@@ -1,10 +1,14 @@
 package controllers
 
+import play.api.cache.Cached
 import play.api.mvc._
 
 class Static extends Controller {
 
-  def index = Action { implicit request =>
-    Ok(views.html.index())
-  }
+  def index =
+    Cached("static-index") {
+      Action { implicit request =>
+        Ok(views.html.index())
+      }
+    }
 }
